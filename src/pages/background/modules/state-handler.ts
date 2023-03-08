4580c8d6de75e14cfbb8a13d6c854f99fe6b57dd
Dinh-Van Colomban <van.colomban@gmail.com>
2023-03-08 12:40:37 +0100

    fix(login): fix auto-login on update

--- src/pages/background/modules/state-handler.ts
+++ src/pages/background/modules/state-handler.ts
@@ -2,7 +2,7 @@ import { catchError, of, switchMap, tap } from 'rxjs';
 
 import { localGet } from '@dvcol/web-extension-utils';
 
-import type { SettingsSlice, StateSlice } from '@src/models';
+import type { StateSlice } from '@src/models';
 import { LoggerService, QueryService } from '@src/services';
 import { restoreState } from '@src/store/actions';
 import { stateSlice } from '@src/store/slices/state.slice';
@@ -14,12 +14,12 @@ import type { Store } from 'redux';
  * @param state the restored state slice
  * @param settings the restored settings slice
  */
-export const restoreLoginSate = (state?: StateSlice, settings?: SettingsSlice) => {
+export const restoreLoginSate = () => {
   // If service is not initialized with url
   if (!QueryService.isReady) return of(null);
 
   // Attempt to Restore login
-  return QueryService.autoLogin({ state, settings, notify: false }).pipe(
+  return QueryService.autoLogin({ notify: false }).pipe(
     tap(() => LoggerService.debug('Login state restored.')),
     catchError(() => of(null)),
   );
