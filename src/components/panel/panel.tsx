8f92a3b91e136ce5d2718d74c57885b3d2c15cd2
Dinh-Van Colomban <van.colomban@gmail.com>
2023-04-07 14:04:24 +0200

    fix(build): move to lazy loaded routes

--- src/components/panel/panel.tsx
+++ src/components/panel/panel.tsx
@@ -14,11 +14,22 @@ import { Config } from './config';
 import { ContentPanel, TaskAdd } from './content';
 import { Settings } from './settings';
 
-import type { FC } from 'react';
+import type { FC, ReactNode } from 'react';
+
+type RouteDefinition = { path: string; element: ReactNode };
+const routes: RouteDefinition[] = [
+  { path: `${AppRoute.All}`, element: <ContentPanel /> },
+  { path: `${AppRoute.Add}/*`, element: <TaskAdd cardProps={{ sx: { m: '0.5rem', fontSize: '1rem' } }} allowFile={true} /> },
+  { path: `${AppRoute.Scrape}/*`, element: <ScrapePanel cardProps={{ sx: { m: '0.5rem' } }} /> },
+  { path: `${AppRoute.Settings}/*`, element: <Settings /> },
+  { path: `${AppRoute.Config}/*`, element: <Config cardProps={{ sx: { m: '0.3rem' } }} /> },
+  { path: `${AppRoute.About}/*`, element: <About cardProps={{ sx: { m: '0.3rem' } }} /> },
+];
 
 export const Panel: FC<{ redirect?: string }> = ({ redirect }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   useEffect(() => {
     if (redirect) {
       dispatch(setNavbar());
@@ -29,12 +40,9 @@ export const Panel: FC<{ redirect?: string }> = ({ redirect }) => {
   return (
     <Container disableGutters maxWidth={false} sx={{ overflow: 'auto', height: 'calc(100% - 3em)' }}>
       <Routes>
-        <Route path={`${AppRoute.All}`} element={<ContentPanel />} />
-        <Route path={`${AppRoute.Add}/*`} element={<TaskAdd cardProps={{ sx: { m: '0.5rem', fontSize: '1rem' } }} allowFile={true} />} />
-        <Route path={`${AppRoute.Scrape}/*`} element={<ScrapePanel cardProps={{ sx: { m: '0.5rem' } }} />} />
-        <Route path={`${AppRoute.Settings}/*`} element={<Settings />} />
-        <Route path={`${AppRoute.Config}/*`} element={<Config cardProps={{ sx: { m: '0.3rem' } }} />} />
-        <Route path={`${AppRoute.About}/*`} element={<About cardProps={{ sx: { m: '0.3rem' } }} />} />
+        {routes?.map(({ path, element }, index) => (
+          <Route key={index} path={path} element={element} />
+        ))}
       </Routes>
     </Container>
   );
