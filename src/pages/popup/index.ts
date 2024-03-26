b7b7df96f238d777c1d73845d0513a41a63803e3
Dinh-Van Colomban <van.colomban@gmail.com>
2024-03-27 00:23:18 +0100

    fix(ui): resize popup after initialisation to prevent undersized window

--- src/pages/popup/index.ts
+++ src/pages/popup/index.ts
@@ -3,15 +3,19 @@ import { initApp } from '@src/pages/common';
 import { LoggerService } from '@src/services';
 import { getPopup } from '@src/store/selectors';
 
-const root = document?.getElementById('synology-download-popup-app-container');
+const resizeContainer = () => {
+  const root = document?.getElementById('synology-download-popup-app-container');
 
-if (root) {
+  if (!root) return;
   root.style.width = `${window.innerWidth}px`;
   root.style.height = `${window.innerHeight}px`;
-}
+};
 
 initApp(ServiceInstance.Popup, AppInstance.popup, getPopup)
-  .then(() => LoggerService.debug('Popup app initialised.'))
+  .then(() => {
+    resizeContainer();
+    LoggerService.debug('Popup app initialised.');
+  })
   .catch(err => LoggerService.debug('Popup app failed to initialised.', err));
 
 if (module.hot) module.hot.accept();
