230a6f8bd5d677e9b00fc6e87aa0ad04ba9b2f8b
Dinh-Van Colomban <van.colomban@gmail.com>
2024-04-08 17:09:15 +0200

    fix(popup): restrict max size for non mac OS popups

--- src/pages/popup/index.ts
+++ src/pages/popup/index.ts
@@ -2,11 +2,18 @@ import { AppInstance, ServiceInstance } from '@src/models';
 import { initApp } from '@src/pages/common';
 import { LoggerService } from '@src/services';
 import { getPopup } from '@src/store/selectors';
+import { isMacOs } from '@src/utils';
 
 const resizeContainer = () => {
   const root = document?.getElementById('synology-download-popup-app-container');
 
   if (!root) return;
+
+  if (!isMacOs()) {
+    root.style.maxWidth = `720px`;
+    root.style.maxHeight = `592px`;
+  }
+
   root.style.width = `${window.innerWidth}px`;
   root.style.height = `${window.innerHeight}px`;
 };
